3827494ca106867c56b18cbdd53cd33047ce5f20
Aliaksandr Neuhen <aliaksandr_neuhen@epam.com>
2016-08-16 18:24:41 +0300

    temp

---
+++ life-ui/app/controllers/HomeController.scala
@@ -0,0 +1,9 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+class HomeController extends Controller {
+  def index = Action { implicit request =>
+    views.html.index()
+  }
+}
---
+++ life-ui/app/life/LifeRouter.scala
@@ -0,0 +1,8 @@
+package life
+
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+class LifeRouter extends SimpleRouter {
+  override def routes: Routes = ???
+}
